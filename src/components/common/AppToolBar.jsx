20444b36d4e0963a1eeaa11b0beeeeafe7ef4cdd
ThandaAye <thanda.aye03@gmail.com>
2020-02-24 21:05:46 +0800

    added add flight form

--- src/components/common/AppToolBar.jsx
+++ src/components/common/AppToolBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Typography, Toolbar, makeStyles } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 const useStyles = makeStyles(theme => ({
   menuButton: {
     marginRight: theme.spacing(2),
@@ -18,7 +18,8 @@ const AppToolBar = props => {
           <MenuIcon />
         </IconButton>
         <Typography variant="h6">
-          GoWhere
+          TOKIGAMES FLIGHT
+          <FlightTakeoffIcon style={{marginLeft: '10px'}} />
         </Typography>
       </Toolbar>
     </AppBar>
